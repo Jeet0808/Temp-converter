f6f81657a181930759ee2887b1ca652e77c11d6f
JEET SOLANKI <solankijeet285@gmail.com>
2024-07-11 16:45:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ function convert(){
     if(tof.checked){
        temp = Number(textbox.value);
        temp = temp * 9/5 + 32;
-       result.textContent = temp.toFixed(1);
+       result.textContent = temp.toFixed(1) + " f";
     }
     else if(toc.checked){
         temp = Number(textbox.value);
          temp = (temp - 32) * (5/9);
-        result.textContent = temp.toFixed(1);
+        result.textContent = temp.toFixed(1) + " c";
     }
     else{
         result.textContent = "select a unit ";
